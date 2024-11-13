b38532efb4574b853253d462f234c50b89b530ba
KeiraDiaz <nkeiradiaz@gmail.com>
2024-11-13 12:01:51 +0700

    Finished Tutorial 8

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:mental_health_tracker/screens/moodentry_form.dart';
+// Impor drawer widget
 import 'package:mental_health_tracker/widgets/left_drawer.dart';
 import 'package:mental_health_tracker/widgets/mood_card.dart';
 
 class MyHomePage extends StatelessWidget {
-  final String npm = '2306256394'; // NPM
-  final String name = 'Keira Diaz Nabhani'; // Name
-  final String className = 'PBP KKI'; // Class
+  final String npm = '2306172325'; // NPM
+  final String name = 'Andriyo Averill Fahrezi'; // Name
+  final String className = 'PBD'; // Class
+
   final List<ItemHomepage> items = [
     ItemHomepage("View Mood", Icons.mood),
     ItemHomepage("Add Mood", Icons.add),
     ItemHomepage("Logout", Icons.logout),
   ];
+
   MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Scaffold provides the basic structure of the page with the AppBar and body.
+    // Scaffold menyediakan struktur dasar halaman dengan appBar dan body.
     return Scaffold(
-      // AppBar is the top part of the page that displays the title.
       appBar: AppBar(
-        // The title of the application "Mental Health Tracker" with white text and bold font.
         title: const Text(
           'Mental Health Tracker',
           style: TextStyle(
@@ -28,21 +28,17 @@ class MyHomePage extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
-        // The background color of the AppBar is obtained from the application theme color scheme.
+        // Mengganti warna icon drawer menjadi putih
+        iconTheme: const IconThemeData(color: Colors.white),
         backgroundColor: Theme.of(context).colorScheme.primary,
-                     // Set drawer icon color to white
-        iconTheme: const IconThemeData(color: Colors.white),   
       ),
-            // Add drawer as a parameter value for the drawer attribute of the Scaffold widget
+      // Masukkan drawer sebagai parameter nilai drawer dari widget Scaffold
       drawer: const LeftDrawer(),
-      // Body of the page with paddings around it.
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        // Place the widget vertically in a column.
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            // Row to display 3 InfoCard horizontally.
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -51,17 +47,10 @@ class MyHomePage extends StatelessWidget {
                 InfoCard(title: 'Class', content: className),
               ],
             ),
-
-            // Give a vertical space of 16 units.
             const SizedBox(height: 16.0),
-
-            // Place the following widget in the center of the page.
             Center(
               child: Column(
-                // Place the text and grid item vertically.
-
                 children: [
-                  // Display the welcome message with bold font and size 18.
                   const Padding(
                     padding: EdgeInsets.only(top: 16.0),
                     child: Text(
@@ -72,18 +61,13 @@ class MyHomePage extends StatelessWidget {
                       ),
                     ),
                   ),
-
-                  // Grid to display ItemCard in a 3 column grid.
                   GridView.count(
                     primary: true,
                     padding: const EdgeInsets.all(20),
                     crossAxisSpacing: 10,
                     mainAxisSpacing: 10,
                     crossAxisCount: 3,
-                    // To ensure that the grid fits its height.
                     shrinkWrap: true,
-
-                    // Display ItemCard for each item in the items list.
                     children: items.map((ItemHomepage item) {
                       return ItemCard(item);
                     }).toList(),
@@ -99,24 +83,18 @@ class MyHomePage extends StatelessWidget {
 }
 
 class InfoCard extends StatelessWidget {
-  // Card information that displays the title and content.
-
-  final String title; // Card title.
-  final String content; // Card content.
+  final String title;
+  final String content;
 
   const InfoCard({super.key, required this.title, required this.content});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      // Create a card box with a shadow.
       elevation: 2.0,
       child: Container(
-        // Set the size and spacing within the card.
-        width: MediaQuery.of(context).size.width /
-            3.5, // Adjust with the width of the device used.
+        width: MediaQuery.of(context).size.width / 3.5,
         padding: const EdgeInsets.all(16.0),
-        // Place the title and content vertically.
         child: Column(
           children: [
             Text(
@@ -131,7 +109,3 @@ class InfoCard extends StatelessWidget {
     );
   }
 }
-
-
-
-
--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mental_health_tracker/screens/menu.dart';
 import 'package:mental_health_tracker/screens/moodentry_form.dart';
-
+import 'package:mental_health_tracker/screens/list_moodentry.dart';
 
 class LeftDrawer extends StatelessWidget {
   const LeftDrawer({super.key});
@@ -12,57 +12,68 @@ class LeftDrawer extends StatelessWidget {
       child: ListView(
         children: [
           DrawerHeader(
-               decoration: BoxDecoration(
-      color: Theme.of(context).colorScheme.primary,
-    ),
-    child: const Column(
-      children: [
-        Text(
-          'Mental Health Tracker',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-        ),
-        Padding(padding: EdgeInsets.all(8)),
-        Text(
-            "Track your mental health every day here!",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-            fontSize: 15,
-            fontWeight: FontWeight.normal,
-            color: Colors.white,
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primary,
             ),
-        ),
-      ],
-    ),
+            child: const Column(
+              children: [
+                Text(
+                  'Mental Health Tracker',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Padding(padding: EdgeInsets.all(8)),
+                Text(
+                  "Track your mental health every day here!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
           ),
-  ListTile(
-    leading: const Icon(Icons.home_outlined),
-    title: const Text('Home Page'),
-    // Redirection part to MyHomePage
-    onTap: () {
-      Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (context) => MyHomePage(),
-          ));
-    },
-  ),
-  ListTile(
-    leading: const Icon(Icons.mood),
-    title: const Text('Add Mood'),
-    // Redirection part to MoodEntryFormPage
-    onTap: () {
-      Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-        builder: (context) => MoodEntryFormPage(),
-          ));
-    },
-  ),
+          ListTile(
+            leading: const Icon(Icons.home_outlined),
+            title: const Text('Home'),
+            // Bagian redirection ke MyHomePage
+            onTap: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MyHomePage(),
+                  ));
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.mood),
+            title: const Text('Add Mood'),
+            // Bagian redirection ke MoodEntryFormPage
+            onTap: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MoodEntryFormPage(),
+                  ));
+            },
+          ),
+          // ListTile Menu code
+          ListTile(
+              leading: const Icon(Icons.add_reaction_rounded),
+              title: const Text('Mood List'),
+              onTap: () {
+                  // Route to the mood page
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const MoodEntryPage()),
+                  );
+              },
+          ),
         ],
       ),
     );
